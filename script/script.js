bf248f159852e225ef672e710d0f9ebeba373b3c
manish-d-art <76776194mm@gmail.com>
2022-08-11 18:30:10 +0530

    update script

--- script/script.js
+++ script/script.js
@@ -1,15 +1,32 @@
 'use strict'
 
-const rate1=document.querySelector('.rate-1');
-const rate2=document.querySelector('.rate-2');
-const rate3=document.querySelector('.rate-3');
-const rate4=document.querySelector('.rate-4');
-const rate5=document.querySelector('.rate-5');
 const button=document.querySelector('.btn');
 const container1=document.querySelector('.container');
 const container2=document.querySelector('.container-thank-you');
 
-rate1.addEventListener('click',function(){
-    container1.classList.add('hidden');
-    container2.classList.remove('hidden');
-})
+let submitted=false;
+
+function init(){
+    for( let i=1;i<=5;i++){
+        document.querySelector(`.rate-${i}`).style.backgroundColor="hsl(216, 12%, 54%, 0.07)";
+    }
+}
+ function rateFunction(val){
+    init();
+     submitted=true;
+    document.querySelector(`.rate-block`).textContent=`you selectet ${val} out of 5`;
+    document.querySelector(`.rate-${val}`).style.backgroundColor='hsl(217, 12%, 63%)';
+}
+
+for(let i=1;i<=5;i++){
+    document.querySelector(`.rate-${i}`).addEventListener('click',function(){
+        rateFunction(i);
+    });
+}
+
+button.addEventListener('click',function(){
+    if(submitted){
+        container1.classList.add('hidden');
+      container2.classList.remove('hidden');
+    }
+});
